936bdfa74830aa0e447dc467c38750115a0228c8
tom <tommir3@qq.com>
2020-10-20 22:39:07 +0800

    界面布局设置，未完成

--- app/src/main/java/com/fecesmemo/tom/fecesmemo/MainActivity.java
+++ app/src/main/java/com/fecesmemo/tom/fecesmemo/MainActivity.java
@@ -7,9 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment[] _viewFragments;      //子页面数组
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         initUI();
     }
 
+    //初始化UI界面
     private void initUI(){
+        initFragments();
         Button btnChart = findViewById(R.id.btnChart);
         Button btnAdd = findViewById(R.id.btnAdd);
         Button btnList = findViewById(R.id.btnList);
@@ -26,20 +28,30 @@ public class MainActivity extends AppCompatActivity {
         btnChart.setOnClickListener(buttonOnClickListener);
         btnList.setOnClickListener(buttonOnClickListener);
     }
-
+    //初始化子页面
+    private void initFragments(){
+        _viewFragments = new Fragment[]{
+                new Chart_Layout(),new Feces_Information(),new List_Layout()
+        };
+        FragmentManager fraManager = getSupportFragmentManager();
+        FragmentTransaction fraTrans = fraManager.beginTransaction();
+        fraTrans.add(R.id.viewContent,new Chart_Layout());
+        fraTrans.commit();
+    }
+    //按钮点击事件
     View.OnClickListener buttonOnClickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
             Fragment curFragment = null;
             switch(v.getId()){
                 case R.id.btnChart:
-                    curFragment = new Chart_Layout();
+                    curFragment = _viewFragments[0];
                     break;
                 case R.id.btnAdd:
-                    curFragment = new Feces_Information();
+                    curFragment = _viewFragments[1];
                     break;
                 case R.id.btnList:
-                    curFragment = new List_Layout();
+                    curFragment = _viewFragments[2];
                     break;
                 default:
                     break;
@@ -47,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
             if(null != curFragment){
                 FragmentManager fraManager = getSupportFragmentManager();
                 FragmentTransaction fraTrans = fraManager.beginTransaction();
-                fraTrans.replace(R.id.fragment,curFragment);
+                fraTrans.replace(R.id.viewContent,curFragment);
                 fraTrans.commit();
             }
         }
