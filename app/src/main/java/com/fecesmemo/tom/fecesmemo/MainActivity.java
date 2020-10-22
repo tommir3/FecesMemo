64bcb21f008134eed3ac69719b983fcb55b0b176
tom <tommir3@qq.com>
2020-10-22 16:00:47 +0800

    图表显示简单测试数据

--- app/src/main/java/com/fecesmemo/tom/fecesmemo/MainActivity.java
+++ app/src/main/java/com/fecesmemo/tom/fecesmemo/MainActivity.java
@@ -1,15 +1,22 @@
 package com.fecesmemo.tom.fecesmemo;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Fragment[] _viewFragments;      //子页面数组
+    private LineChart chart;        //图表控件
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
     //初始化UI界面
     private void initUI(){
-        initFragments();
         Button btnChart = findViewById(R.id.btnChart);
         Button btnAdd = findViewById(R.id.btnAdd);
         Button btnList = findViewById(R.id.btnList);
         btnAdd.setOnClickListener(buttonOnClickListener);
         btnChart.setOnClickListener(buttonOnClickListener);
         btnList.setOnClickListener(buttonOnClickListener);
-    }
-    //初始化子页面
-    private void initFragments(){
-        _viewFragments = new Fragment[]{
-                new Chart_Layout(),new Feces_Information(),new List_Layout()
-        };
-        FragmentManager fraManager = getSupportFragmentManager();
-        FragmentTransaction fraTrans = fraManager.beginTransaction();
-        fraTrans.add(R.id.viewContent,new Chart_Layout());
-        fraTrans.commit();
+        initChart();
     }
     //按钮点击事件
     View.OnClickListener buttonOnClickListener = new View.OnClickListener(){
@@ -57,11 +54,31 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
             if(null != curFragment){
-                FragmentManager fraManager = getSupportFragmentManager();
-                FragmentTransaction fraTrans = fraManager.beginTransaction();
-                fraTrans.replace(R.id.viewContent,curFragment);
-                fraTrans.commit();
+//                FragmentManager fraManager = getSupportFragmentManager();
+//                FragmentTransaction fraTrans = fraManager.beginTransaction();
+//                fraTrans.replace(R.id.viewContent,curFragment);
+//                fraTrans.commit();
             }
         }
     };
+
+    private void initChart(){
+        chart = findViewById(R.id.chart);
+        List dataList = new ArrayList();
+        dataList.add(new Entry(20201005,1));
+        dataList.add(new Entry(20201009,3));
+        dataList.add(new Entry(20201010,1));
+        dataList.add(new Entry(20201011,2));
+        dataList.add(new Entry(20201015,1));
+        dataList.add(new Entry(20201016,3));
+        dataList.add(new Entry(20201017,1));
+        dataList.add(new Entry(20201018,2));
+        LineDataSet lineDataSet=new LineDataSet(dataList,null);
+        lineDataSet.setLineWidth(2f);
+        lineDataSet.setCircleRadius(5f);
+        LineData lineData=new LineData(lineDataSet);
+        chart.setData(lineData);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+    }
 }
